bcaa3c589c239c9d42c63eac0f553c8dbb86f45f
Santosh2514122 <santoshgkp107@gmail.com>
2024-09-28 10:13:00 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float C,F;
+    printf("enter the temprature in Celsius:");
+    scanf("%f",&C);
+F=(C*9)/5+32;
+printf("\n tempracher in farenheigh is %f",F);
+return 0;
+    
+}
